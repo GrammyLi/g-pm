f2fbf9e595afb8d42eaf72028f32e3b507265e48
魔法师 <grammyli@outlook.com>
2021-11-29 13:02:23 +0800

    remve axios

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,12 +1,12 @@
 /*
  * @Author: your name
  * @Date: 2021-11-29 12:52:19
- * @LastEditTime: 2021-11-29 12:52:19
+ * @LastEditTime: 2021-11-29 13:01:59
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /g-pm/src/api/fetchWeather.js
  */
-import axios from 'axios';
+// import axios from 'axios';
 
 const URL = 'https://api.openweathermap.org/data/2.5/weather';
 const API_KEY = 'f33a484cf794d08d0148764789aaba32';
@@ -30,14 +30,14 @@ const dealWeather = data => {
     }
 }
 
-export const fetchWeather = async (query) => {
-    const { data } = await axios.get(URL, {
-        params: {
-            q: query,
-            units: 'metric',
-            APPID: API_KEY,
-        }
-    });
+// export const fetchWeather = async (query) => {
+//     const { data } = await axios.get(URL, {
+//         params: {
+//             q: query,
+//             units: 'metric',
+//             APPID: API_KEY,
+//         }
+//     });
 
-    return dealWeather(data);
-}
+//     return dealWeather(data);
+// }
