db3edd118e3aa9d86e70984f2462c7e653a9906e
魔法师 <grammyli@outlook.com>
2021-12-29 14:34:33 +0800

    add

--- src/page/exam/ExamplePage.js
+++ src/page/exam/ExamplePage.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-11-29 12:24:12
- * @LastEditTime: 2021-12-28 20:16:53
+ * @LastEditTime: 2021-12-29 14:34:21
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /g-pm/src/page/exam/ExamplePage.js
@@ -11,7 +11,7 @@ import Test from "./Test"
 import Test2 from "./Test2"
 import Test3 from "./Test3"
 // import Button from "../../componets/Button";
-import { log } from "../../static/js/utils";
+// import { log } from "../../static/js/utils";
 // import Input from "../../componets/Input";
 
 // import "./SearchPage.css";
